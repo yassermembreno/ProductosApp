18a916bb6d5dc2fdd2b7969192e99b26dc4826e8
yassermembreno <yass.membreo.gudiel@gmail.com>
2021-09-07 10:38:54 -0600

    Added CRUD to ProductoModel

---
+++ Domain/Enums/UnidadMedida.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Enums
+{
+    public enum UnidadMedida
+    {
+        Unidades,
+        Litros,
+        MiliLitros,
+        Kilogramos,
+        Gramos,
+        Libras
+    }
+}
---
+++ Infraestructure/Productos/ProductoModel.cs
@@ -0,0 +1,123 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestructure.Productos
+{
+    public class ProductoModel
+    {
+        private Producto[] productos;
+
+        #region CRUD
+        public void Add(Producto p)
+        {
+            Add(p, ref productos);
+        }
+
+        public int Update(Producto p)
+        {
+            if(p == null)
+            {
+                throw new ArgumentException("El producto no puede ser null.");
+            }
+
+            int index = GetIndexById(p.Id);
+            if(index < 0)
+            {
+                throw new Exception($"El producto con id {p.Id} no se encuentra.");
+            }
+
+            productos[index] = p;
+            return index;
+        }
+
+        public bool Delete(Producto p)
+        {
+            if (p == null)
+            {
+                throw new ArgumentException("El producto no puede ser null.");
+            }
+
+            int index = GetIndexById(p.Id);
+            if (index < 0)
+            {
+                throw new Exception($"El producto con id {p.Id} no se encuentra.");
+            }
+
+            if(index != productos.Length - 1)
+            {
+                productos[index] = productos[productos.Length - 1];
+            }
+
+            Producto[] tmp = new Producto[productos.Length - 1];
+            Array.Copy(productos, tmp, tmp.Length);
+            productos = tmp;
+
+            return productos.Length == tmp.Length;
+        }
+        public Producto[] GetAll()
+        {
+            return productos;
+        }
+
+        #endregion
+
+        #region Queries
+        public Producto GetProductoById(int id)
+        {
+            if(id <= 0)
+            {
+                throw new ArgumentException($"El Id: {id} no es valido.");
+            }
+
+            int index = GetIndexById(id);            
+
+            return index <= 0 ? null : productos[index];
+        }
+        #endregion
+
+        #region Private Method
+        private void Add(Producto p, ref Producto[] pds)
+        {
+            if(pds == null)
+            {
+                pds = new Producto[1];
+                pds[pds.Length - 1] = p;
+                return;
+            }
+
+            Producto[] tmp = new Producto[pds.Length + 1];
+            Array.Copy(pds, tmp, pds.Length);
+            tmp[tmp.Length - 1] = p;
+            pds = tmp;
+        }
+
+        private int GetIndexById(int id)
+        {
+            if(id <= 0)
+            {
+                throw new ArgumentException("El id no puede ser negativo o cero.");
+            }
+
+            int index = int.MinValue, i = 0;
+            if(productos == null)
+            {
+                return index;
+            }
+
+            foreach (Producto p in productos)
+            {
+                if(p.Id == id)
+                {
+                    index = i;
+                    break;
+                }
+                i++;
+            }
+
+            return index;
+        }
+        #endregion
+    }
+}
